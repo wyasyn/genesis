c8ab1cff3e41818788c333a82fb3a71d2f064a06
Yasin Walum <ywalum@gmail.com>
2025-10-26 02:02:02 +0300

    create  dev branch

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,5 +1,12 @@
 const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <section className="container min-h-screen flex flex-col justify-center">
+      <h1 className="text-center text-9xl">Full Stack Developer</h1>
+      <p className="text-center">
+        website under construction... come back soon!
+      </p>
+    </section>
+  );
 };
 
 export default Hero;
